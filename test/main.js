d964eb1986e0a97a74901092c6d8da97eaa262cf
tylerthayn <tylerthayn@hotmail.com>
2022-10-13 00:30:10 -0600

    '.'

--- test/main.js
+++ test/main.js
@@ -1,17 +1,6 @@
-# jquery.properties
-jQuery extension for setting data properties with listeners
-
-## Installation
-
-```sh
-npm install @tyler.thayn/jquery.properties
-```
-
-## Usage
-
-```js
 require(['jquery', '../jquery.properties.js'], ($) => {
 	$(() => {
+
 		$('.Test').Property('MyProp', function (...args) {
 			console.log(this)
 			console.log(args)
@@ -25,12 +14,9 @@ require(['jquery', '../jquery.properties.js'], ($) => {
 			console.log(args)
 		})
 
+
 		$('.Test').Property('MyProp', 'MyVal')
 		$('.Test').data({OtherProp: 115, AnotherProp: true})
+
 	})
 })
-```
-
-```js
-fetch('https://cdn.jsdelivr.net/npm/@tyler.thayn/jquery.properties@latest/jquery.properties.js').then(t => t.text()).then(eval)
-```
